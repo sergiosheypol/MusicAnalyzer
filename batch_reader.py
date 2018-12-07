2db9cdd10318a1973512bf9300e8e299b6a8a10c
Sergio López <sergiolf96@gmail.com>
2018-12-07 21:01:51 +0100

    Improved analyzer. Support for already analyzed JSON files added + several security checkings

--- batch_reader.py
+++ batch_reader.py
@@ -22,11 +22,8 @@ class BatchReader():
         # Average LUFS value of the analyzed files
         self.average_lufs = None
 
+    # Analyze a music folder
     def run(self):
-        # Auxiliar lists. It helps simplify calculating average value
-        lufs_aux = []
-        tp_aux = []
-
         # Analyzer
         for track in self.files:
             measures = AudioMeters.get_loudness(self.path + '/' + track)
@@ -43,32 +40,113 @@ class BatchReader():
             self.lufs_database.append(lufs)
             self.tp_database.append(tp)
 
-            # Preparing values to be computed
-            lufs_aux.append(measures['Integrated Loudness']['I'])
-            tp_aux.append(measures['True Peak']['Peak'])
-
             print(track + " has been added successfully")
 
         # Calculate the mean value of each parameter
-        self.average_lufs = {
-            'avg_lufs': np.mean(lufs_aux)
-        }
-        self.average_tp = {
-            'avg_tp': np.mean(tp_aux)
-        }
+        self.calculate_avg_lufs()
+        self.calculate_avg_tp()
 
+    # Export the results into JSON
     def export(self, folder_path):
         fc = FileConverter(folder_path)
 
         # Checking if everything's not null
-        if self.lufs_database != []:
+        if self.lufs_database:
             fc.py_to_json(self.lufs_database, 'lufs.json')
 
-        if self.tp_database != []:
+        if self.tp_database:
             fc.py_to_json(self.tp_database, 'tp.json')
 
-        if self.average_lufs != None:
+        if self.average_lufs:
             fc.py_to_json(self.average_lufs, 'avg_lufs.json')
 
-        if self.average_tp != None:
+        if self.average_tp:
             fc.py_to_json(self.average_tp, 'avg_tp.json')
+
+    # Add an existing LUFS database
+    def add_existing_lufs_database(self, folder_path, file):
+
+        if not os.path.exists(folder_path):
+            print('The folder does not exist')
+            return False
+
+        if not os.path.isfile(file):
+            print('The file does not exist')
+            return False
+
+        # Create JSON converter
+        fc = FileConverter(folder_path)
+
+        # Create aux database
+        lufs_database = fc.json_to_py(file)
+
+        # Move it to self object
+        self.lufs_database = lufs_database
+
+        print('LUFS database imported correctly')
+
+        return True
+
+    # Add an existing TP database
+    def add_existing_tp_database(self, folder_path, file):
+
+        if not os.path.exists(folder_path):
+            print('The folder does not exist')
+            return False
+
+        if not os.path.isfile(file):
+            print('The file does not exist')
+            return False
+
+        # Create JSON converter
+        fc = FileConverter(folder_path)
+
+        # Create aux database
+        tp_database = fc.json_to_py(file)
+
+        # Move it to self object
+        self.tp_database = tp_database
+
+        print('TP database imported correctly')
+
+        return True
+
+    # Calculate the average lufs value from a given database
+    def calculate_avg_lufs(self):
+
+        if not self.lufs_database:
+            return False
+
+        values = []
+
+        # Read all tracks in database
+        for track in self.lufs_database:
+            value = list(track.values())
+            values.append(value)
+
+        # Calculate the avg LUFS value
+        self.average_lufs = {
+            'avg_lufs': np.mean(values)
+        }
+
+        return True
+
+    # Calculate the average tp value from a given database
+    def calculate_avg_tp(self):
+
+        if not self.tp_database:
+            return False
+
+        values = []
+
+        # Read all tracks in database
+        for track in self.tp_database:
+            value = list(track.values())
+            values.append(value)
+
+        # Calculate the avg TP value
+        self.average_tp = {
+            'avg_tp': np.mean(values)
+        }
+
+        return True
--- tests_sergio.py
+++ tests_sergio.py
@@ -7,12 +7,50 @@
 
 from batch_reader import BatchReader
 
-reader = BatchReader('audio_files/edm')
-reader.run()
+# EDM
+# reader = BatchReader('audio_files/edm')
+# reader.run()
+# reader.export('output/edm')
+# print(reader.average_tp)
+# print(reader.average_lufs)
+# print(reader.tp_database)
 
-print(reader.tp_database)
-print(reader.lufs_database)
-print(reader.average_tp)
-print(reader.average_lufs)
+# # ROCK
+# reader = BatchReader('audio_files/rock')
+# reader.run()
+# reader.export('output/rock')
+# print(reader.average_tp)
+# print(reader.average_lufs)
 
-reader.export('output/edm')
+
+# Test adding database functions
+
+# reader2 = BatchReader('audio_files/edm')
+# reader2.add_existing_lufs_database('output/edm', 'lufs.json')
+# reader2.add_existing_tp_database('output/edm', 'tp.json')
+# a1 = reader2.calculate_avg_lufs()
+# a2 = reader2.calculate_avg_tp()
+
+
+# print(reader2.lufs_database)
+# print(reader2.tp_database)
+# print(reader2.average_tp)
+# print(reader2.average_lufs)
+
+
+# reader3 = BatchReader('audio_files/rock')
+# reader3.add_existing_lufs_database('output/rock', 'lufs.json')
+# reader3.add_existing_tp_database('output/rock', 'tp.json')
+# reader3.calculate_avg_tp()
+# reader3.calculate_avg_lufs()
+# print(reader3.average_lufs)
+# print(reader3.average_tp)
+
+reader4 = BatchReader('audio_files/rock')
+reader4.add_existing_lufs_database('output/edm', 'lufs.json')
+# reader4.add_existing_tp_database('output/edm', 'tp.json')
+#
+# reader4.run()
+# reader4.export('output/mix')
+#
+# print(reader4.tp_database)
