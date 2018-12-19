009f0224c3d07b79ab4b8c079a5a694e78079105
Sergio López <sergiolf96@gmail.com>
2018-12-19 13:59:03 +0100

    Pandas is now integrated

--- data_analyzer.py
+++ data_analyzer.py
@@ -1,4 +1,5 @@
 from audio_meters import AudioMeters
+import numpy as np
 import os
 import pandas as pd
 from pathlib import Path
@@ -37,48 +38,46 @@ class DataAnalyzer:
 
         self.genres_database = self.genres_database.drop_duplicates(keep='first')
 
-
     # Exports DB to JSON file
     def save_database(self):
         if isinstance(self.genres_database, pd.DataFrame):
             self.genres_database.to_json(self.database_file_path, orient='index')
 
     # Calculates the genre with the minimum distance between vectors
-    # def calculate_genre(self, track_path):
-    #     if not os.path.isfile(track_path):
-    #         return 'The file does not exist'
-    #
-    #     # Given track parameters
-    #     measures = AudioMeters.get_loudness(track_path)
-    #     lufs = measures['Integrated Loudness']['I']
-    #     tp = measures['True Peak']['Peak']
-    #     track_values = [lufs, tp]
-    #
-    #     # Aux vars for the loop
-    #     most_alike_genre = None
-    #     min_distance = None
-    #
-    #     # Search for the most likely genre
-    #     for genre in self.genres_database:
-    #
-    #         # Genre parameters
-    #         genre_values = [genre['lufs'], genre['tp']]
-    #
-    #         # Distance between genres
-    #         subs = np.subtract(track_values, genre_values)
-    #         distance = np.linalg.norm(subs)
-    #         print(distance)
-    #
-    #         # If it is the first iteration, set that genre as most alike
-    #         if not min_distance:
-    #             min_distance = distance
-    #             most_alike_genre = genre['name']
-    #             continue
-    #
-    #         # If the current distance is smaller than the previous, switch them
-    #         if min_distance > distance:
-    #             min_distance = distance
-    #             most_alike_genre = genre['name']
-    #             continue
-    #
-    #     return most_alike_genre
+    def calculate_genre_euclidean_distance(self, track_path):
+        if not os.path.isfile(Path(track_path)):
+            return 'The file does not exist'
+
+        # Given track parameters
+        measures = AudioMeters.get_loudness(track_path)
+        lufs = measures['Integrated Loudness']['I']
+        tp = measures['True Peak']['Peak']
+        track_values = [lufs, tp]
+
+        # Aux vars for the loop
+        most_alike_genre = None
+        min_distance = None
+
+        # Search for the most likely genre
+        for genre, values in self.genres_database.iterrows():
+            # Genre parameters
+            genre_values = [values.lufs, values.tp]
+
+            # Distance between genres
+            subs = np.subtract(track_values, genre_values)
+            distance = np.linalg.norm(subs)
+            print(distance)
+
+            # If it is the first iteration, set that genre as most alike
+            if not min_distance:
+                min_distance = distance
+                most_alike_genre = genre
+                continue
+
+            # If the current distance is smaller than the previous, switch them
+            if min_distance > distance:
+                min_distance = distance
+                most_alike_genre = genre
+                continue
+
+        return most_alike_genre
